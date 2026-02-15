a261886e48fc61c02034b9523ed2909f0fc5fd11
frifa <frifa@FRIDA>
2026-02-14 20:26:09 -0600

    Agregar archivos de proyecto.

---
+++ IngresoPython/IngresoPython.py
@@ -0,0 +1,74 @@
+﻿import tkinter as tk
+from tkinter import messagebox
+
+def borrar():
+    tbNombre.delete(0, tk.END)
+    tbApellido.delete(0, tk.END)
+    tbEdad.delete(0, tk.END)
+    tbTelefono.delete(0, tk.END)
+    tbEstatura.delete(0, tk.END)
+    rbSeleccionGenero.set(0)
+
+def guardar():
+    nombres = tbNombre.get()
+    apellidos = tbApellido.get()
+    edad = tbEdad.get()
+    tel = tbTelefono.get()
+    estatura = tbEstatura.get()
+    genero = ""
+
+    if rbSeleccionGenero.get()==1:
+        genero="Masculino"
+    elif rbSeleccionGenero.get()==2:
+        genero="Femenino"
+    elif rbSeleccionGenero.get()==3:
+        genero="Otro"
+    datos = f"Nombre: {nombres}\nApellido: {apellidos}\nEdad: {edad}"
+    + "\nTelefono: {tel}\nEstatura: {estatura}\nGenero: {genero}"
+    with open ("Datos3MRegistro","a") as archivo:
+        archivo.write(datos+"\n\n")
+    messagebox.showinfo("Información", "Datos guardados correctamente\n"
+                        +datos)
+
+ventana = tk.Tk()
+ventana.configure(bg="gray")
+ventana.geometry("350x550")
+ventana.title("Actividad 04 - Formulario de Registro")
+
+rbSeleccionGenero = tk.IntVar()
+
+tk.Label(ventana, text="Nombre: ", font=("Times New Roman", 14, "" )).pack(padx=10, pady=5)
+tbNombre = tk.Entry(ventana, width=35, justify="center")
+tbNombre.pack(padx=10, pady=5)
+tk.Label(ventana, text="Apellido: ", font=("Times New Roman", 14, "" )).pack(padx=10, pady=5)
+tbApellido = tk.Entry(ventana, width=35, justify="center")
+tbApellido.pack(padx=10, pady=5)
+tk.Label(ventana, text="Edad: ", font=("Times New Roman", 14, "" )).pack(padx=10, pady=5)
+tbEdad = tk.Entry(ventana, width=35, justify="center")
+tbEdad.pack(padx=10, pady=5)
+tk.Label(ventana, text="Estatura: ", font=("Times New Roman", 14, "" )).pack(padx=10, pady=5)
+tbEstatura = tk.Entry(ventana, width=35, justify="center")
+tbEstatura.pack(padx=10, pady=5)
+tk.Label(ventana, text="Teléfono: ", font=("Times New Roman", 14, "" )).pack(padx=10, pady=5)
+tbTelefono = tk.Entry(ventana, width=35, justify="center")
+tbTelefono.pack(padx=10, pady=5)
+
+gb = tk.LabelFrame(ventana, text="Seleccione genero: ", padx=10, pady=8)
+gb.pack(padx=10,pady=8)
+
+rbMasculino = tk.Radiobutton(gb, text="Masculino", value=1, variable=rbSeleccionGenero)
+rbMasculino.grid(column=1, row=1)
+rbFemenino = tk.Radiobutton(gb, text="Femenino", value=2, variable=rbSeleccionGenero)
+rbFemenino.grid(column=2, row=1)
+rbOtro = tk.Radiobutton(gb, text="Otro", value=3, variable=rbSeleccionGenero)
+rbOtro.grid(column=3, row=1)
+
+gb2 = tk.LabelFrame(ventana, text="")
+gb2.pack(padx=10,pady=8)
+
+btnBorrar = tk.Button(gb2, text="Borrar", width=10, padx=10, pady=8, command=borrar)
+btnBorrar.grid(column=2, row=1)
+btnGuardar = tk.Button(gb2, text="Guardar", width=10, padx=10, pady=8, command=guardar)
+btnGuardar.grid(column=1, row=1)
+
+ventana.mainloop()
