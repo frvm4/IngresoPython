869741d11085c4d48e5adeddb8dcd8575c67acf2
Frida Velázquez <frifa0608@gmail.com>
2026-04-13 23:25:06 -0600

    Meter a base de datos

--- IngresoPython/IngresoPython.py
+++ IngresoPython/IngresoPython.py
@@ -1,5 +1,40 @@
 ﻿import tkinter as tk
 from tkinter import messagebox
+import re
+import mysql.connector
+
+def insertarRegistro(nombre, apellido, edad, estatura, telefono,genero):
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost", user="root", password="4444", database="formulario")
+        cursor = conexion.cursor()
+        query = "INSERT INTO registro (nombre, apellido, edad, estatura, telefono, genero) VALUES (%s, %s, %s, %s, %s, %s)"
+        valores = (nombre, apellido, edad, estatura, telefono, genero)
+        cursor.execute(query, valores)
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        messagebox.showinfo("Información", "Datos guardados en la base de datos con éxito.")
+    except mysql.connector.Error as err:
+        messagebox.showerror("Error", f"Error al insertar los datos:{err}")
+
+def EnteroValido(valor):
+    try:
+        int(valor)
+        return True
+    except ValueError:
+        messagebox.showerror("Error Edad","Valor de edad equivocado")
+        return False
+def DecimalValido(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+def TelefonoValido(valor):
+    return valor.isdigit() and len(valor)==10
+def TextoValido(valor):
+    return bool(re.match("^[a-zA-Z\\s]+$", valor))
 
 def borrar():
     tbNombre.delete(0, tk.END)
@@ -23,11 +58,12 @@ def guardar():
         genero="Femenino"
     elif rbSeleccionGenero.get()==3:
         genero="Otro"
-    datos = f"Nombre: {nombres}\nApellido: {apellidos}\nEdad: {edad}"
-    + "\nTelefono: {tel}\nEstatura: {estatura}\nGenero: {genero}"
-    with open ("Datos3MRegistro","a") as archivo:
-        archivo.write(datos+"\n\n")
-    messagebox.showinfo("Información", "Datos guardados correctamente\n"
+    if (EnteroValido(edad) and DecimalValido(estatura)and TelefonoValido(tel)and TextoValido(nombres) and TextoValido(apellidos)):
+        datos = f"Nombre: {nombres}\nApellido: {apellidos}\nEdad: {edad} \nTelefono: {tel}\nEstatura: {estatura}\nGenero: {genero}"
+        with open ("Datos3MRegistro","a") as archivo:
+            insertarRegistro(nombres, apellidos, edad, estatura, tel, genero)
+            archivo.write(datos+"\n\n")
+        messagebox.showinfo("Información", "Datos guardados correctamente\n"
                         +datos)
 
 ventana = tk.Tk()
